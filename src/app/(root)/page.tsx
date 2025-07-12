33a530db4f07c4646870f534b0631d2301dc274b
devnuckles <mohammadroman925@gmail.com>
2025-07-12 17:54:44 +0600

    add hero section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,9 @@
-import DynamicButton from "@/components/common/dynamic.button.component";
+import Hero from "@/components/platform/home/hero.component";
 
 export default function HomePage() {
     return (
-        <main className="h-screen flex justify-center items-center">
-            <h1 className="text-4xl text-center font-bold mb-8">
-                Welcome to <br /> GymBro!
-            </h1>
-
-            <DynamicButton btnText="Explore Now" href="/sign-in" />
-        </main>
+        <>
+            <Hero />
+        </>
     );
 }
